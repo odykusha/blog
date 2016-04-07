121c51f85bf3482311584757d18c3cbfb419f8dd
odykusha <dxn@ukr.net>
2016-04-07 19:15:10 +0300

    auto add tags for links

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-import urllib.request
+import requests
 import sys
 
 def get_value(word, left_symb, right_symb):
@@ -9,41 +9,33 @@ def get_value(word, left_symb, right_symb):
 
 def get_tag(some_link):
     try:
-        req = urllib.request.Request(some_link)
-        response = urllib.request.urlopen(req)
-        #response = urllib.request.urlopen(some_link)
-    except urllib.error.URLError:
-        print('||*|| except', sys.exc_info()[0])
-        return some_link
+        response = requests.get(some_link)
+    # except urllib.error.URLError:
+    #     print('||*|| except', sys.exc_info()[0])
+    #     return some_link
     except:
         print('||**|| except', sys.exc_info()[0])
         return some_link
-    headers = response.info()
-    CONTENT_TYPE = headers.get('Content-type')
-    CODER = 'utf-8'
-    if 'charset' in headers.get('Content-type'):
-        CODER = headers.get('Content-type').split('; charset=')[1]
+    CONTENT_TYPE = response.headers['content-type']
     # якщо силка на сайт
     if 'text/html' in CONTENT_TYPE:
         print('Content-type: text/html', some_link)
-        data = response.read()
-        sdata = data.decode(CODER)
-        title = get_value(sdata, '<title>', '</title>')
+        title = get_value(response.text, '<title>', '</title>')
         # якщо ютюб
         if 'YouTube' in title:
             print('YouTube: Content-type: text/html', some_link)
-            new_link = response.geturl()\
+            new_link = response.url\
                 .replace('www.youtube.com/watch?v=', 'www.youtube.com/embed/')\
                 .replace('&nohtml5=False','')
             res = title + '\n'
-            res += "<a href='" + response.geturl() + "' target=\"_blank\">" + response.geturl() + "</a>" + '\n'
+            res += "<a href='" + response.url + "' target=\"_blank\">" + response.url + "</a>" + '\n'
             res += '<iframe width="560" height="315" src="' + new_link + '" frameborder="0" allowfullscreen></iframe>'
         # якщо коуб
-        elif 'coub.com' in response.geturl().lower():
+        elif 'coub.com' in response.url.lower():
             print('COUB: Content-type: text/html', some_link)
-            new_link = response.geturl().replace('coub.com/view/', 'coub.com/embed/')
+            new_link = response.url.replace('coub.com/view/', 'coub.com/embed/')
             res = title + '\n'
-            res += "<a href='" + response.geturl() + "' target=\"_blank\">" + response.geturl() + "</a>" + '\n'
+            res += "<a href='" + response.url + "' target=\"_blank\">" + response.url + "</a>" + '\n'
             res += '<iframe src="' + new_link + '?muted=false&autostart=false&originalSize=false&startWithHD=true" allowfullscreen="true" frameborder="0" width="640" height="294"></iframe>'
         # якщо всі інші сайти
         else:
@@ -53,14 +45,14 @@ def get_tag(some_link):
     # якщо ссилка на інший формат: js, css та інше
     elif 'text' in CONTENT_TYPE:
         print('Content-type: text', some_link)
-        return "<a href='" + response.geturl() + "' target=\"_blank\">" + response.geturl() + "</a>"
+        return "<a href='" + response.url + "' target=\"_blank\">" + response.url + "</a>"
     # для картинок
     elif 'image' in CONTENT_TYPE:
         print('Content-type: image/..', some_link)
-        res = "<a href='" + response.geturl() + "'>" + response.geturl() + "</a>" + '\n'
-        res += "<img src='" + response.geturl() + "'>"
+        res = "<a href='" + response.url + "'>" + response.url + "</a>" + '\n'
+        res += "<img src='" + response.url + "'>"
         return res
     # якщо нідочого не відповідає
     else:
         print('some else', some_link)
-        return response.geturl()
+        return response.url
